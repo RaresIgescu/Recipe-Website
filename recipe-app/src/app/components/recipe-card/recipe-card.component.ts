1671256f46088975b4f31252f8dedb71270e26e0
Rares Igescu <raresigescu@gmail.com>
2025-05-03 14:52:49 +0300

    Redesign recipe-card

--- recipe-app/src/app/components/recipe-card/recipe-card.component.ts
+++ recipe-app/src/app/components/recipe-card/recipe-card.component.ts
@@ -2,10 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Recipe } from '../../interfaces/recipe.interface';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-recipe-card',
-  imports: [CommonModule],
+  imports: [CommonModule, CardModule, ButtonModule],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.scss',
 })
