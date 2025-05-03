226de29c7d2229767a64b958399d4c424119cd0a
Rares Igescu <raresigescu@gmail.com>
2025-05-03 14:52:24 +0300

    Remove Irrelevant Imports

--- recipe-app/src/app/app.config.ts
+++ recipe-app/src/app/app.config.ts
@@ -5,9 +5,6 @@ import { providePrimeNG}  from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import Aura from '@primeng/themes/aura';
-import Material from '@primeng/themes/material';
-import Lara from '@primeng/themes/lara'; 
-import Nora from '@primeng/themes/nora';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
