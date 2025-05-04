ab108441a11987e34460b2be212e021b15e4e2db
Rares Igescu <raresigescu@gmail.com>
2025-05-04 15:28:40 +0300

    Design recipe-page

--- recipe-app/src/app/recipe-page/recipe-page.component.ts
+++ recipe-app/src/app/recipe-page/recipe-page.component.ts
@@ -4,24 +4,43 @@ import { Recipe } from '../interfaces/recipe.interface';
 import { RecipesService } from '../services/recipes.service';
 import { db } from '../db/db';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
+import { FooterComponent } from "../components/footer/footer.component";
 
 @Component({
   selector: 'app-recipe-page',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule, FooterComponent],
   templateUrl: './recipe-page.component.html',
   styleUrl: './recipe-page.component.scss'
 })
 export class RecipePageComponent {
   recipeId!: string;
-  recipe?: Recipe;
+  recipeChosen?: Recipe;
+  recipes!: Recipe[];
+  errorMessage: any = ' ';
 
-  constructor(private route: ActivatedRoute, private recipesService: RecipesService) {}
+  constructor(private route: ActivatedRoute, private recipesService: RecipesService, readonly router: Router) {}
 
   ngOnInit() {
     this.recipeId = this.route.snapshot.paramMap.get('id')!;
-    this.recipe = this.recipesService.getRecipeById(this.recipeId);
+  
+    setInterval(() => {
+      db.subscribeQuery({ recipes: {} }, (resp) => {
+        if (resp.error) {
+          this.errorMessage = resp.error;
+        }
+    
+        if (resp.data) {
+          this.recipes = resp.data.recipes;
+    
+          this.recipeChosen = this.recipes.find(r => r.id === this.recipeId);
+        }
+      });
+    }, 100)
   }
 
-  readonly router = inject(ActivatedRoute);
-  
+  backToCatalogue() {
+    this.router.navigateByUrl('/recipes');
+  }
 }
